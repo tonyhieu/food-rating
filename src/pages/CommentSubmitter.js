0e8c595fc28aaa64e8a34a80d9203cf81b7ebca1
Brian Kim <bhk3164@gmail.com>
2023-04-22 14:21:00 -0700

    Worked on comments and creating slider component

---
+++ src/pages/CommentSubmitter.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../App.css';
+
+function CommentSubmitter() {
+    const navigate = useNavigate();
+    const [message, setMessage] = useState();
+
+    return (
+        <div className="comment-card">
+            /* put slider */
+        </div>
+    );
+}
+
+
+export default CommentSubmitter
+
+
+
+
+
+
+
+
