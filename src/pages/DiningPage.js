45d3680cfe635c90f86bd87f6d06b52316ea997f
tonyhieu <54915685+tonyhieu@users.noreply.github.com>
2023-04-22 15:52:39 -0700

    change css

--- src/pages/DiningPage.js
+++ src/pages/DiningPage.js
@@ -22,6 +22,51 @@ function DiningPage(props) {
                 ]
             },
             {
+                "name": "De Neve",
+                "averageRating": 4.3,
+                "imageUrl": "https://conferences.ucla.edu/wp-content/uploads/2019/01/De-Neve-Plaza_JH.jpg",
+                "comments": [
+                    "the food is okay",
+                    "i liked the breakfast"
+                ]
+            },
+            {
+                "name": "De Neve",
+                "averageRating": 4.3,
+                "imageUrl": "https://conferences.ucla.edu/wp-content/uploads/2019/01/De-Neve-Plaza_JH.jpg",
+                "comments": [
+                    "the food is okay",
+                    "i liked the breakfast"
+                ]
+            },
+            {
+                "name": "De Neve",
+                "averageRating": 4.3,
+                "imageUrl": "https://conferences.ucla.edu/wp-content/uploads/2019/01/De-Neve-Plaza_JH.jpg",
+                "comments": [
+                    "the food is okay",
+                    "i liked the breakfast"
+                ]
+            },
+            {
+                "name": "De Neve",
+                "averageRating": 4.3,
+                "imageUrl": "https://conferences.ucla.edu/wp-content/uploads/2019/01/De-Neve-Plaza_JH.jpg",
+                "comments": [
+                    "the food is okay",
+                    "i liked the breakfast"
+                ]
+            },
+            {
+                "name": "De Neve",
+                "averageRating": 4.3,
+                "imageUrl": "https://conferences.ucla.edu/wp-content/uploads/2019/01/De-Neve-Plaza_JH.jpg",
+                "comments": [
+                    "the food is okay",
+                    "i liked the breakfast"
+                ]
+            },
+            {
                 "name": "Epicurious",
                 "averageRating": 4.6,
                 "imageUrl": "https://portal.housing.ucla.edu/sites/default/files/media/images/Epicuria%20at%20Covel4_square.png",
@@ -47,7 +92,7 @@ function DiningPage(props) {
         }
 
         cards.push(
-            <div className="card">
+            <div className="dining-hall-card">
                 <img src={hall.imageUrl} alt="Hall Image" />
                 <h2>{hall.name}</h2>
                 <p className="dining-rating">Average rating: {hall.averageRating}</p>
@@ -66,7 +111,7 @@ function DiningPage(props) {
     return (
         <div className="dining-page">
             <h1 className="dining-title">Dining Halls at {schoolName}</h1>
-            <div className="dining-card">
+            <div className="dining-cards">
                 {cards}
             </div>
 
