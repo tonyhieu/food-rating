d23cfa416c6fd4420aa127e8e3ec85d49322e736
tonyhieu <54915685+tonyhieu@users.noreply.github.com>
2023-04-22 14:21:20 -0700

    add search

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import './App.css';
 import HomePage from './pages/HomePage.js'
 import DiningPage from './pages/DiningPage.js'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 function Home() {
@@ -12,6 +14,7 @@ function Home() {
             <h1 className="home-header">Dining Review</h1>
             <form>
                 <input type="text" name="message" id="input-message" placeholder="Type a school name here..." />
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
             </form>
         </div>
     );
